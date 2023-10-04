816234f4e568613d450a33011be21db0f83f46e4
Chelsea Nzebo bonnet <chelseanzebobonnet@MacBook-Pro-de-Chelsea.local>
2023-10-04 12:22:16 +0200

    ex_03.php

---
+++ ex_03/ex_03.php
@@ -0,0 +1,17 @@
+<?php
+$my_array = array("to",42,"Glory",42.42,"Geckos
+");
+
+print_r($my_array);
+
+
+
+
+
+
+
+// $string = "to";
+// $integer = 42;
+// $string = "Glory";
+// $float = 42.42;
+// $string = "Geckos"
